16afea59377a94de1ec82ec83eeb32b8904e58db
NickyGon <60781642+NickyGon@users.noreply.github.com>
2021-05-24 17:03:21 -0400

    Ejercicio de Singleton

---
+++ singleton/ejercicios/LibreCambista.java
@@ -0,0 +1,10 @@
+package singleton.ejercicios;
+
+public class LibreCambista {
+    public LibreCambista(){
+        EmpresaDeCambio.getInstance().info("Contactando Libre Cambista");
+    }
+    public void cambiarDinero(int cantidad,String tipo,String objetivo){
+        EmpresaDeCambio.getInstance().getChange("LC",cantidad,tipo,objetivo);
+    }
+}
